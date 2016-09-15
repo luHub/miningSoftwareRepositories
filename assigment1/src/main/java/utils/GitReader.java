37c59b467366d327279bc00ba5a0fa3198e22ea6
Giannis Pap <Giannis Pap@Giannis>
2016-09-15 22:33:16 +0200

    some corrections

--- assigment1/src/main/java/utils/GitReader.java
+++ assigment1/src/main/java/utils/GitReader.java
@@ -30,8 +30,9 @@ public class GitReader {
             InputStreamReader isr = new InputStreamReader(is);
             BufferedReader br = new BufferedReader(isr);
             String line;
-            while ((line = br.readLine()) != null && PathFilters.checkPathFilterEnding(line,"java")) {
-               listOfPaths.add(line);
+            while ((line = br.readLine()) != null) {
+            	if(PathFilters.checkPathFilterEnding(line,"java"))
+            		listOfPaths.add(line);
             }
             int exitValue = process.waitFor();
             
@@ -46,13 +47,14 @@ public class GitReader {
 	/**
 	 * Read from Git the using the shortlog command for a desired repository,
 	 * directly to a cache.
+	 * @param until2 
 	 * @throws IOException 
 	 */
-	public static Map<String, String> readFileCommitsFromDevelopers(String path, String since, String until) throws IOException {
+	public static Map<String, String> readFileCommitsFromDevelopers(String pathProject,String path, String since, String until) throws IOException {
 		Map<String, String> devInformationMap = new HashMap<String, String>();
 		String[] command = { "CMD", "/C", "git shortlog -sn --since=" + since + "--until=" + until + " -p" + path };
 		ProcessBuilder probuilder = new ProcessBuilder(command);
-		probuilder.directory(new File(path));
+		probuilder.directory(new File(pathProject));
 			Process process = probuilder.start();
 			// Read out dir output
 			InputStream is = process.getInputStream();
