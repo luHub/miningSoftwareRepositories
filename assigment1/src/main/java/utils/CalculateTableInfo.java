7df1b2c3ee25c77a2f105e8bf8daa10f65eff515
Giannis Pap <Giannis Pap@Giannis>
2016-09-15 22:02:07 +0200

    some corrections in calculate methods

--- assigment1/src/main/java/utils/CalculateTableInfo.java
+++ assigment1/src/main/java/utils/CalculateTableInfo.java
@@ -8,13 +8,13 @@ public class CalculateTableInfo {
 	private final static double LOWER_LIMIT = 5.0;
 	private final static double ONE_HUNDRED_PERCENTAGE = 100.0;
 
-	public static int calculateTotalContributors(Map<String,String> mapof){
-		return mapof.size();
+	public static int calculateTotalContributors(Map<String,String> devInformationMap){
+		return devInformationMap.size();
 	}
 	
-	public static int calculateTotalCommitsPerFile(Map<String,String> mapof){
+	public static int calculateTotalCommitsPerFile(Map<String,String> devInformationMap){
 		int totalCommits=0;
-		for (Map.Entry<String, String> entry : mapof.entrySet())
+		for (Map.Entry<String, String> entry : devInformationMap.entrySet())
 		{
 			 totalCommits += Integer.parseInt("entry.getKey()");
 		}
@@ -22,40 +22,41 @@ public class CalculateTableInfo {
 		return totalCommits;
 	}
 	
-	public static int calculateMinorContributors(Map<String,String> mapof){
+	
+	public static int calculateMinorContributors(Map<String,String> devInformationMap){
 		int minorCounter=0;
-		for (Map.Entry<String, String> entry : mapof.entrySet())
+		for (Map.Entry<String, String> entry : devInformationMap.entrySet())
 		{
-			double proportion = (Integer.parseInt(entry.getKey()) * calculateTotalCommitsPerFile(mapof))/ONE_HUNDRED_PERCENTAGE;
+			double proportion = (Integer.parseInt(entry.getKey()) * calculateTotalCommitsPerFile(devInformationMap))/ONE_HUNDRED_PERCENTAGE;
 			if(proportion < LOWER_LIMIT/ONE_HUNDRED_PERCENTAGE)
 				minorCounter++;
 		}
 		
-		return minorCounter++;
+		return minorCounter;
 	}
 	
-	public static int calculateMajorContributors(Map<String,String> mapof){
+	public static int calculateMajorContributors(Map<String,String> devInformationMap){
 		int majorCounter=0;
-		for (Map.Entry<String, String> entry : mapof.entrySet())
+		for (Map.Entry<String, String> entry : devInformationMap.entrySet())
 		{
-			double proportion = (Integer.parseInt(entry.getKey()) * calculateTotalCommitsPerFile(mapof))/ONE_HUNDRED_PERCENTAGE;
+			double proportion = (Integer.parseInt(entry.getKey()) * calculateTotalCommitsPerFile(devInformationMap))/ONE_HUNDRED_PERCENTAGE;
 			if(proportion >= LOWER_LIMIT/ONE_HUNDRED_PERCENTAGE && proportion < UPER_LIMIT/ONE_HUNDRED_PERCENTAGE)
 				majorCounter++;
 		}
 		
-		return majorCounter++;
+		return majorCounter;
 	}
 	
-	public static double calculateOwnership(Map<String,String> mapof){
+	public static double calculateOwnership(Map<String,String> devInformationMap){
 		double ownerCounter=0;
-		for (Map.Entry<String, String> entry : mapof.entrySet())
+		for (Map.Entry<String, String> entry : devInformationMap.entrySet())
 		{
-			double proportion = (Integer.parseInt(entry.getKey()) * calculateTotalCommitsPerFile(mapof))/ONE_HUNDRED_PERCENTAGE;
+			double proportion = (Integer.parseInt(entry.getKey()) * calculateTotalCommitsPerFile(devInformationMap))/ONE_HUNDRED_PERCENTAGE;
 			if(proportion >= UPER_LIMIT/ONE_HUNDRED_PERCENTAGE)
 				ownerCounter += proportion;
 		}
 		
-		return ownerCounter++;
+		return ownerCounter;
 	}
 	
 }
