4b3ce4dbb4c5fefe93f9d5f62e4c1ce0389011f6
lguerchi <lucioguerchi16f@gmail.com>
2016-09-15 18:04:45 +0200

    Added filter to get files from git by extension

--- assigment1/src/main/java/main.java
+++ assigment1/src/main/java/main.java
@@ -4,5 +4,4 @@ public class main {
 	public static void main(String args[]){
 		System.out.println("Hello World");
 	}
-	
 }
--- assigment1/src/main/java/utils/PathFilters.java
+++ assigment1/src/main/java/utils/PathFilters.java
@@ -1,5 +1,20 @@
 package utils;
 
-public class PathFilters {
+import java.util.regex.Pattern;
 
+public class PathFilters {
+	
+	/**
+	 * returns true if the path contains the mathed extension,
+	 * the extension must not include the period (.).
+	 * Example:
+	 * (.../foo/example.java , java)
+	 * @param regex
+	 * @return 
+	 */
+	public static boolean checkPathFilterEnding(String path,String extension){
+		return Pattern.matches(".*\\."+extension, path);
+	}
+	
+	
 }
