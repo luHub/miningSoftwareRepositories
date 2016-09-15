4545a053cbfe3d0a1eec7bc70c032d38746f5512
Giannis Pap <Giannis Pap@Giannis>
2016-09-15 18:11:49 +0200

    merged files and git reader method

--- assigment1/src/main/java/utils/GitReader.java
+++ assigment1/src/main/java/utils/GitReader.java
@@ -1,5 +1,39 @@
 package utils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class GitReader {
 
+	static List<String> readGitPaths(){
+		List<String> listOfPaths = new ArrayList<String>();
+		String[] command = {"CMD", "/C", "git ls-files"};
+	    ProcessBuilder probuilder = new ProcessBuilder( command );
+	    probuilder.directory(new File("C:\\Users\\Giannis Pap\\Lucene\\lucene-solr"));
+	    Process process;
+        try {
+            process = probuilder.start();
+            //Read out dir output
+            InputStream is = process.getInputStream();
+            InputStreamReader isr = new InputStreamReader(is);
+            BufferedReader br = new BufferedReader(isr);
+            String line;
+            while ((line = br.readLine()) != null) {
+               listOfPaths.add(line);
+            }
+            //Wait to get exit value
+            int exitValue = process.waitFor();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (IOException ex) {
+            	ex.printStackTrace();            
+			}
+	    return listOfPaths ;
+	}
 }
